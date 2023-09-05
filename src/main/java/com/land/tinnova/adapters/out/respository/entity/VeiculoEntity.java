2393f59bbd7757b95d499df936793a5b3fa6b193
landson90 <randelsilva1990@gmail.com>
2023-09-05 18:12:24 -0300

    criando emtity de veiculo

--- src/main/java/com/land/tinnova/adapters/out/respository/entity/VeiculoEntity.java
+++ src/main/java/com/land/tinnova/adapters/out/respository/entity/VeiculoEntity.java
@@ -1,16 +1,36 @@
 package com.land.tinnova.adapters.out.respository.entity;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+
+import java.time.OffsetDateTime;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_veiculo")
 public class VeiculoEntity {
 
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
     private Long id;
     private String marca;
     private Integer ano;
     private String descicao;
     private Boolean vendido;
 
-    private LocalDateTime createAt;
-    private LocalDateTime updateAt;
+    @CreationTimestamp
+    @Column(nullable = false, columnDefinition = "datetime")
+    private OffsetDateTime createAt;
+    @UpdateTimestamp
+    @Column(nullable = false, columnDefinition = "datetime")
+    private OffsetDateTime updateAt;
 
 }
